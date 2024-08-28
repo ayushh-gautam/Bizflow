e80d2d45f45285283e8d960d5f2c6624ebba903c
ayushh-gautam <ayushgautam896@gmail.com>
2024-08-28 08:06:05 +0545

    feat : ui updated

--- lib/features/login/main/home/home_page.dart
+++ lib/features/login/main/home/home_page.dart
@@ -1,13 +1,37 @@
-import 'package:bizflow/common/components/text.dart';
+import 'package:bizflow/features/login/main/settings/notification_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../config/routes/imports.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    final customColors = Theme.of(context).extension<AppColors>()!;
+    return Scaffold(
+      backgroundColor: customColors.bgSecondary,
+      appBar: AppBar(
+        title: CustomText(
+          text: 'Home',
+          myStyle: headingheadinglg.copyWith(
+            color: customColors.textDefault,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const NofificationPage();
+              }));
+            },
+            child: SvgPicture.asset(SvgAsset.notification)
+                .addMargin(EdgeInsets.only(right: 12.w)),
+          )
+        ],
+      ),
+      body: const Center(
         child: CustomText(text: 'HomePage'),
       ),
     );
