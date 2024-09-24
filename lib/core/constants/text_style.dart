1c706947173e4dff7aab74d1c4dd77ef4542a229
ayushh-gautam <ayushgautam896@gmail.com>
2024-09-24 22:35:57 +0545

    fix: font family issue fixed

--- lib/core/constants/text_style.dart
+++ lib/core/constants/text_style.dart
@@ -1,232 +1,209 @@
 import 'package:flutter/material.dart';
-  TextStyle get headingheading3xl => const TextStyle(
-    fontSize: 36,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 48 / 36,
-    letterSpacing: -0.55,
-  );
+import 'package:google_fonts/google_fonts.dart';
 
-  TextStyle get headingheading2xl => const TextStyle(
-    fontSize: 32,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 44 / 32,
-    letterSpacing: -0.31,
-  );
+TextStyle get headingheading3xl => GoogleFonts.manrope(
+      fontSize: 36,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 48 / 36,
+      letterSpacing: -0.55,
+    );
 
-  TextStyle get headingheadingxl => const TextStyle(
-    fontSize: 24,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 32 / 24,
-    letterSpacing: -0.21,
-  );
+TextStyle get headingheading2xl => GoogleFonts.manrope(
+      fontSize: 32,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 44 / 32,
+      letterSpacing: -0.31,
+    );
 
+TextStyle get headingheadingxl => GoogleFonts.manrope(
+      fontSize: 24,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 32 / 24,
+      letterSpacing: -0.21,
+    );
 
-  TextStyle get headingheadinglg => const TextStyle(
-    fontSize: 20,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 28 / 20,
-    letterSpacing: -0.21,
-  );
+TextStyle get headingheadinglg => GoogleFonts.manrope(
+      fontSize: 20,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 28 / 20,
+      letterSpacing: -0.21,
+    );
 
-  TextStyle get headingheadingmd => const TextStyle(
-    fontSize: 18,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 28 / 18,
-    letterSpacing: 0,
-  );
+TextStyle get headingheadingmd => GoogleFonts.manrope(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 28 / 18,
+      letterSpacing: 0,
+    );
 
-  TextStyle get headingheadingsm => const TextStyle(
-    fontSize: 16,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 24 / 16,
-    letterSpacing: 0,
-  );
+TextStyle get headingheadingsm => GoogleFonts.manrope(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodylgdefault => const TextStyle(
-    fontSize: 18,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Regular',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w400,
-    height: 28 / 18,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodylgdefault => GoogleFonts.manrope(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 28 / 18,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodylgunderline => const TextStyle(
-    fontSize: 18,
-    decoration: TextDecoration.underline,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 28 / 18,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodylgunderline => GoogleFonts.manrope(
+      fontSize: 18,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 28 / 18,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodylgmedium => const TextStyle(
-    fontSize: 18,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 28 / 18,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodylgmedium => GoogleFonts.manrope(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 28 / 18,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodylgsemibold => const TextStyle(
-    fontSize: 18,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Semi Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 28 / 18,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodylgsemibold => GoogleFonts.manrope(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 28 / 18,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodymddefault => const TextStyle(
-    fontSize: 16,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Regular',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w400,
-    height: 24 / 16,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodymddefault => GoogleFonts.manrope(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodymdunderline => const TextStyle(
-    fontSize: 16,
-    decoration: TextDecoration.underline,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 24 / 16,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodymdunderline => GoogleFonts.manrope(
+      fontSize: 16,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodymdmedium => const TextStyle(
-    fontSize: 16,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 24 / 16,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodymdmedium => GoogleFonts.manrope(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodymdmediumunderline => const TextStyle(
-    fontSize: 16,
-    decoration: TextDecoration.underline,
-    fontFamily: 'Inter-Semi Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 24 / 16,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodymdmediumunderline => GoogleFonts.manrope(
+      fontSize: 16,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodymdsemibold => const TextStyle(
-    fontSize: 16,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Semi Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 24 / 16,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodymdsemibold => GoogleFonts.manrope(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodysmdefault => const TextStyle(
-    fontSize: 14,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Regular',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w400,
-    height: 20 / 14,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodysmdefault => GoogleFonts.manrope(
+      fontSize: 14,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 20 / 14,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodysmunderline => const TextStyle(
-    fontSize: 14,
-    decoration: TextDecoration.underline,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 20 / 14,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodysmunderline => GoogleFonts.manrope(
+      fontSize: 14,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 20 / 14,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodysmmedium => const TextStyle(
-    fontSize: 14,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 20 / 14,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodysmmedium => GoogleFonts.manrope(
+      fontSize: 14,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 20 / 14,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodysmmediumunderline => const TextStyle(
-    fontSize: 14,
-    decoration: TextDecoration.underline,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 20 / 14,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodysmmediumunderline => GoogleFonts.manrope(
+      fontSize: 14,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 20 / 14,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodysmsemibold => const TextStyle(
-    fontSize: 14,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Semi Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 20 / 14,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodysmsemibold => GoogleFonts.manrope(
+      fontSize: 14,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 20 / 14,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodyxsdefault => const TextStyle(
-    fontSize: 12,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Regular',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w400,
-    height: 16 / 12,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodyxsdefault => GoogleFonts.manrope(
+      fontSize: 12,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 16 / 12,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodyxsmedium => const TextStyle(
-    fontSize: 12,
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Medium',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    height: 16 / 12,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodyxsmedium => GoogleFonts.manrope(
+      fontSize: 12,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 16 / 12,
+      letterSpacing: 0,
+    );
 
-  TextStyle get bodybodyxssemibold => const TextStyle(
-    fontSize: 12,
-    
-    decoration: TextDecoration.none,
-    fontFamily: 'Inter-Bold',
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w700,
-    height: 16 / 12,
-    letterSpacing: 0,
-  );
+TextStyle get bodybodyxssemibold => GoogleFonts.manrope(
+      fontSize: 12,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 16 / 12,
+      letterSpacing: 0,
+    );
