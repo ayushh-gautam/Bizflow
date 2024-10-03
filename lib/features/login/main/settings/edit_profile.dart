ad9ea403bf737e944720299ddb586f1ed4703113
ayushh-gautam <ayushgautam896@gmail.com>
2024-10-03 17:44:49 +0545

    feat: functionality added for change photo option

--- lib/features/login/main/settings/edit_profile.dart
+++ lib/features/login/main/settings/edit_profile.dart
@@ -1,14 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:bizflow/config/routes/imports.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
 
-class EditProfile extends StatelessWidget {
+class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  File? image;
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      final tempImage = File(image.path);
+
+      setState(() => this.image = tempImage);
+    } on PlatformException catch (e) {
+      print('failedto pick image $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final customColors = Theme.of(context).extension<AppColors>()!;
-    final nameController = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
@@ -40,7 +61,7 @@ class EditProfile extends StatelessWidget {
                   ),
                   Gap(4.h),
                   CustomTextField(
-                    controller: nameController,
+                    controller: TextEditingController(),
                     placeholder: 'Ayush Gautam',
                   ),
                   Gap(24.h),
@@ -51,7 +72,7 @@ class EditProfile extends StatelessWidget {
                   ),
                   Gap(4.h),
                   CustomTextField(
-                    controller: nameController,
+                    controller: TextEditingController(),
                     placeholder: 'ayushgautam88@gmail.com',
                   ),
                   Gap(24.h),
@@ -62,7 +83,7 @@ class EditProfile extends StatelessWidget {
                   ),
                   Gap(4.h),
                   CustomTextField(
-                    controller: nameController,
+                    controller: TextEditingController(),
                     placeholder: '9811219209',
                   ),
                   Gap(24.h),
@@ -91,19 +112,28 @@ class EditProfile extends StatelessWidget {
         alignment: Alignment.center,
         children: [
           ClipOval(
-            child: Image.network(
-              'https://i.pinimg.com/564x/ae/4e/29/ae4e29ce8a84f7b95d5f27faed37fc9c.jpg',
-              height: 100.h,
-              width: 100.w,
-              fit: BoxFit.cover,
-            ),
-          ),
+              child: image == null
+                  ? Image.network(
+                      'https://i.pinimg.com/564x/ae/4e/29/ae4e29ce8a84f7b95d5f27faed37fc9c.jpg',
+                      height: 100.h,
+                      width: 100.w,
+                      fit: BoxFit.cover,
+                    )
+                  : Image.file(
+                      image!,
+                      height: 100.h,
+                      width: 100.w,
+                      fit: BoxFit.cover,
+                    )),
           Positioned(
             top: 70,
             right: 0,
             left: 12,
             child: GestureDetector(
-              onTap: () => _showEditDialog(context, customColors),
+              onTap: () => _showEditDialog(
+                context,
+                customColors,
+              ),
               child: Container(
                   height: 32.h,
                   width: 71.w,
@@ -132,31 +162,92 @@ class EditProfile extends StatelessWidget {
       ),
     ));
   }
-}
 
-void _showEditDialog(BuildContext context, AppColors customColors) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return Dialog(
-        backgroundColor: customColors.bgBackground!,
-        insetPadding: EdgeInsets.symmetric(horizontal: 18.w),
-        alignment: Alignment.center,
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: List.generate(
-            3,
-            (index) {
-              return const ListTile(
-                leading: Icon(Icons.photo),
-                title: CustomText(text: 'Hahahahh'),
+  void _showEditDialog(BuildContext context, AppColors customColors) {
+    List<String> actionText = ['View Photo', 'Change Photo', 'Remove Photo'];
+
+    List<Widget> leadingIcons = const [
+      Icon(Icons.filter),
+      Icon(Icons.edit),
+      Icon(Icons.delete)
+    ];
+
+    List<VoidCallback> callbacks = [
+      //viwew photo
+      () {},
+
+      //choose photo
+      () {
+        List<String> changeText = ['Take from Camera', 'Choose from gallery'];
+        List<Widget> changeIcons = const [
+          Icon(Icons.camera),
+          Icon(Icons.filter)
+        ];
+
+        List<VoidCallback> actionCallback = [
+          () {
+            Navigator.pop(context);
+            pickImage(ImageSource.camera);
+          },
+          () {
+            Navigator.pop(context);
+            pickImage(ImageSource.gallery);
+          }
+        ];
+        Navigator.pop(context);
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                backgroundColor: customColors.bgBackground,
+                insetPadding: EdgeInsets.symmetric(horizontal: 18.w),
+                alignment: Alignment.center,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.r)),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: List.generate(changeText.length, (index) {
+                    return ListTile(
+                      title: CustomText(
+                        text: changeText[index],
+                      ),
+                      leading: changeIcons[index],
+                      onTap: actionCallback[index],
+                    );
+                  }),
+                ),
               );
-            },
+            });
+      },
+      () {
+        // Remove Photo action logic
+      },
+    ];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: customColors.bgBackground,
+          insetPadding: EdgeInsets.symmetric(horizontal: 18.w),
+          alignment: Alignment.center,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(
+              actionText.length,
+              (index) {
+                return ListTile(
+                  onTap: callbacks[index],
+                  leading: leadingIcons[index],
+                  title: CustomText(text: actionText[index]),
+                );
+              },
+            ),
           ),
-        ),
-      );
-    },
-  );
+        );
+      },
+    );
+  }
 }
