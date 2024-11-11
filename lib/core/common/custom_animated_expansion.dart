333625c6c76cabcd30a8cb0a8759d3a39b74c8a8
ayushh-gautam <ayushgautam896@gmail.com>
2024-11-11 20:48:31 +0545

    feat: animated expansion container created

---
+++ lib/core/common/custom_animated_expansion.dart
@@ -0,0 +1,39 @@
+import 'package:bizflow/config/themes/themeExtension/theme_extension.dart';
+import 'package:flutter/material.dart';
+
+class CustomAnimatedExpansion extends StatelessWidget {
+  const CustomAnimatedExpansion(
+      {super.key,
+      required this.secondChild,
+      required this.isTapped,
+      this.firstChild});
+  final Widget? firstChild;
+  final Widget secondChild;
+  final bool isTapped;
+
+   
+
+  @override
+  Widget build(BuildContext context) {
+    final customColors = Theme.of(context).extension<AppColors>()!;
+    return AnimatedCrossFade(
+      reverseDuration: const Duration(milliseconds: 400),
+      firstChild: firstChild ??
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+          ),
+      secondChild: AnimatedContainer(
+          color: customColors.bgBackground,
+          duration: const Duration(milliseconds: 1000),
+          // height: widget._isTapped ? 500 : 0,
+          width: MediaQuery.of(context).size.width,
+          child: secondChild),
+      firstCurve: const Interval(0.0, 0.6, curve: Curves.fastOutSlowIn),
+      secondCurve: const Interval(0.4, 1.0, curve: Curves.fastOutSlowIn),
+      sizeCurve: Curves.fastOutSlowIn,
+      crossFadeState:
+          isTapped ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+      duration: const Duration(milliseconds: 400),
+    );
+  }
+}
