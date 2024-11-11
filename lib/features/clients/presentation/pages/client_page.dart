e3896ca2ac24e822ecbc024103ee5f59687c8099
ayushh-gautam <ayushgautam896@gmail.com>
2024-11-11 20:51:55 +0545

    list of client displaying completed

--- lib/features/clients/presentation/pages/client_page.dart
+++ lib/features/clients/presentation/pages/client_page.dart
@@ -1,5 +1,8 @@
 import 'package:bizflow/config/routes/imports.dart';
+import 'package:bizflow/features/clients/data/model/client_model.dart';
+import 'package:bizflow/features/clients/presentation/cubit/client_cubit.dart';
 import 'package:bizflow/features/clients/presentation/pages/add_client_page.dart';
+import 'package:bizflow/features/clients/presentation/pages/client_detail_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../widget/user_card.dart';
@@ -21,7 +24,7 @@ class ClientPage extends StatelessWidget {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: CustomButton(
           title: 'Add Client',
-          width: 170.w,
+          width: 170.h,
           titleColor: customColors.brandSecondary!,
           buttonColor: customColors.brandPrimary!,
           leading: Icon(
@@ -40,13 +43,34 @@ class ClientPage extends StatelessWidget {
 
             //searchh icon
 
-            const UserCard(
-                userName: 'Yogesh',
-                email: 'xyz@gmail.com',
-                totalProjects: '10',
-                invoiceAmount: '220000',
-                projectStatus: 'Complete',
-                userImageUrl: ''),
+            BlocBuilder<ClientCubit, List<Client>>(
+              builder: (context, clients) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: clients.length,
+                  itemBuilder: (context, index) {
+                    final client = clients[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return ClientDetail(
+                            clientt: client,
+                          );
+                        }));
+                      },
+                      child: UserCard(
+                          userName: client.fullName,
+                          email: client.email,
+                          totalProjects: 10.toString(),
+                          invoiceAmount: 3000.toString(),
+                          projectStatus: 'Completed',
+                          userImageUrl: ''),
+                    );
+                  },
+                );
+              },
+            ),
           ],
         ).addMargin(EdgeInsets.symmetric(horizontal: 16.w)));
   }
