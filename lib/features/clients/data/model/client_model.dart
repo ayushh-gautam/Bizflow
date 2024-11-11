808d7eb1405a7e346dee618f8c0b0abc6da528c0
ayushh-gautam <ayushgautam896@gmail.com>
2024-11-11 20:50:52 +0545

    model modified

--- lib/features/clients/data/model/client_model.dart
+++ lib/features/clients/data/model/client_model.dart
@@ -3,8 +3,12 @@ class Client {
   final String companyName;
   final String email;
   final String tags;
+  final String phoneNumber;
+  final String address;
 
   Client({
+    required this.address,
+    required this.phoneNumber,
     required this.fullName,
     required this.companyName,
     required this.email,
