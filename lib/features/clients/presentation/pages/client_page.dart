9008040c1244a74ce402416672b5b699b567e060
ayushh-gautam <ayushgautam896@gmail.com>
2024-11-13 22:59:47 +0545

    feat: created the empty label of add client

--- lib/features/clients/presentation/pages/client_page.dart
+++ lib/features/clients/presentation/pages/client_page.dart
@@ -4,6 +4,7 @@ import 'package:bizflow/features/clients/presentation/cubit/client_cubit.dart';
 import 'package:bizflow/features/clients/presentation/pages/add_client_page.dart';
 import 'package:bizflow/features/clients/presentation/pages/client_detail_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../widget/user_card.dart';
 
@@ -22,21 +23,28 @@ class ClientPage extends StatelessWidget {
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: CustomButton(
-          title: 'Add Client',
-          width: 170.h,
-          titleColor: customColors.brandSecondary!,
-          buttonColor: customColors.brandPrimary!,
-          leading: Icon(
-            Icons.add,
-            color: customColors.brandSecondary!,
-          ),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (builder) {
-              return const AddClientPage();
-            }));
-          },
-        ),
+        floatingActionButton:
+            BlocBuilder<ClientCubit, List<Client>>(builder: (context, clients) {
+          if (clients.isEmpty) {
+            return SizedBox();
+          } else {
+            return CustomButton(
+              title: 'Add Client',
+              width: 170.h,
+              titleColor: customColors.brandSecondary!,
+              buttonColor: customColors.brandPrimary!,
+              leading: Icon(
+                Icons.add,
+                color: customColors.brandSecondary!,
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (builder) {
+                  return const AddClientPage();
+                }));
+              },
+            );
+          }
+        }),
         body: Column(
           children: <Widget>[
             Gap(16.h),
@@ -45,30 +53,73 @@ class ClientPage extends StatelessWidget {
 
             BlocBuilder<ClientCubit, List<Client>>(
               builder: (context, clients) {
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: clients.length,
-                  itemBuilder: (context, index) {
-                    final client = clients[index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return ClientDetail(
-                            clientt: client,
-                          );
-                        }));
-                      },
-                      child: UserCard(
-                          userName: client.fullName,
-                          email: client.email,
-                          totalProjects: 10.toString(),
-                          invoiceAmount: 3000.toString(),
-                          projectStatus: 'Completed',
-                          userImageUrl: ''),
-                    );
-                  },
-                );
+                if (clients.isEmpty) {
+                  return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Gap(40.h),
+                        SvgPicture.asset(SvgAsset.emptyState),
+                        Gap(28.h),
+                        CustomText(
+                          text: 'Let\'s Add your first Client',
+                          myStyle: headingheadingmd.copyWith(
+                              color: customColors.textDefault),
+                        ),
+                        Gap(6.h),
+                        CustomText(
+                          textAlign: TextAlign.center,
+                          text:
+                              'Add a new client to get started managing your projects.',
+                          myStyle: bodybodymddefault.copyWith(
+                              color: customColors.textSecondary),
+                        ),
+                        Gap(8.h),
+                        CustomButton(
+                            height: 52.h,
+                            width: 120.h,
+                            style: bodybodymdmedium.copyWith(
+                                color: customColors.brandSecondary),
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return const AddClientPage();
+                                },
+                              ));
+                            },
+                            leading: Icon(
+                              Icons.add,
+                              color: customColors.brandSecondary,
+                            ),
+                            title: 'Add Client',
+                            titleColor: customColors.brandSecondary!,
+                            buttonColor: customColors.brandPrimary!)
+                      ]).addMargin(EdgeInsets.symmetric(horizontal: 16.h));
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: clients.length,
+                    itemBuilder: (context, index) {
+                      final client = clients[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return ClientDetail(
+                              clientt: client,
+                            );
+                          }));
+                        },
+                        child: UserCard(
+                            userName: client.fullName,
+                            email: client.email,
+                            totalProjects: 10.toString(),
+                            invoiceAmount: 3000.toString(),
+                            projectStatus: 'Completed',
+                            userImageUrl: ''),
+                      );
+                    },
+                  );
+                }
               },
             ),
           ],
