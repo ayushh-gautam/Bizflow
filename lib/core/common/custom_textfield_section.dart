22dec670266871d8616929822c7603e8bb259a78
ayushh-gautam <ayushgautam896@gmail.com>
2024-11-24 16:31:37 +0545

    code refractored

---
+++ lib/core/common/custom_textfield_section.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../../config/routes/imports.dart';
+
+
+class TextFieldSection extends StatelessWidget {
+  final String title;
+  final String placeholder;
+  final Widget? trailing;
+  final bool password;
+  final void Function()? trailingTapped;
+  final FocusNode? focusnode;
+  final String? Function(String?)? validator;
+  final TextEditingController controller;
+  TextFieldSection({
+    super.key,
+    this.focusnode,
+    this.trailing,
+    this.password = false,
+    required this.customColors,
+    required this.title,
+    required this.controller,
+    required this.placeholder,
+    this.validator, this.trailingTapped,
+  });
+
+  final AppColors customColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomText(
+          text: title,
+          myStyle: bodybodymdmedium.copyWith(color: customColors.textDefault),
+        ),
+        Gap(4.h),
+        KTextField(
+          focusnode: focusnode,
+          password: password,
+          trailing: trailing,
+          trailingTapped: trailingTapped,
+          controller: controller,
+          placeholder: placeholder,
+          validator: validator,
+          
+        )
+      ],
+    );
+  }
+}
